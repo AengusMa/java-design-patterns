366373099f3b592d09ed4dabad7fcc17662f9b0a
mawenlong <15061886113@163.com>
2018-11-06 17:44:10 +0800

    工具类

---
+++ utils/src/main/java/com/mwl/util/Print.java
@@ -0,0 +1,13 @@
+package com.mwl.util;
+
+/**
+ * @author mawenlong
+ * @date 2018/11/06
+ */
+public class Print {
+
+  public static void separator() {
+    System.out
+        .println("==========================================================================");
+  }
+}
